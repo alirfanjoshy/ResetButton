34447d45ccb707287fdde9dc18b49d5ae8ea41ad
alirfanjoshy <159354995+alirfanjoshy@users.noreply.github.com>
2025-08-29 01:20:05 -0400

    index.js

--- index.js
+++ index.js
@@ -1,120 +1,25 @@
-//console.log(`Hello`)
-//console.log(`I like pizza`);
+//Random number generator
+//const min=50;
+//const max=100;
+//let randomNum=Math.floor(Math.random() * max-min)+min;
 
+//console.log(randomNum);
 
-//window.alert(`this is an alert`);
-//window.alert(`i like pizza`);
+const myButton=document.getElementById("myButton");
+const myLabel=document.getElementById("myLabel");
+const myLabe2=document.getElementById("myLabe2");
+const myLabe3=document.getElementById("myLabe3");
+const min=1;
+const max=6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-//document.getElementById("myH1").textContent=`Hello`;
-//document.getElementById("myP").textContent=`I like pizza`;
-
-//let x;
-//x=100;
-
-//console.log(x);
-
-//let age=25;
-//let price=10.99;
-//let gpa=2.1;
-//let favouriteFood=`pizza`;
-
-//console.log(typeof favouriteFood);
-//console.log(`your favourite food ${favouriteFood} `);
-
-//let online=true;
-//let forSale=true;
-//let isStudent=true;
-
-
-//console.log(typeof online);
-//console.log(`i am online: ${online}`);
-//console.log(`is this car for sale: ${forSale}`);
-//console.log(`Enrolled : ${isStudent}`);
-
-//let fullName="Al Irfan";
-//let age=22;
-//let student=true;
-
-//document.getElementById("p1").textContent=`your name is ${fullName}`;
-//document.getElementById("p2").textContent=`you are ${age} years old`;
-//document.getElementById("p3").textContent=`Enrolled: ${student}`;
-
-//let students=30;
-
-//studentss=students+1;
-//students=sstudents-1;
-//students=students*2;
-//students=students/2;
-//students=students**3;
-//students=students%3;
-//students%=3;
-
-//console.log(students);
-
-//et result=12 % 5 + 8/2;
-
-//console.log(result);
-
-//let usernaem;
-
-//username=window.prompt("What is your user name?");
-//console.log(username);
-
-//let username;
-
-//document.getElementById("mySubmit").onclick=function(){
-//username=document.getElementById("myText").value;
-//document.getElementById("myH1").textContent=`Hello ${username}`;
- 
-//let age=window.prompt("How old are you?");
-//age=Number(age);
-//age+=1;
-//console.log(age,typeof age);
-
-//let x=25;
-//let y="pizza";
-//let z="true";
-
-//x=Number(x);
-//y=String(y);
-//z=Boolean(z);
-
-//console.log(x,typeof x);
-//console.log(y,typeof y);
-//console.log(z,typeof z);
-
-//const PI=3.14159;
-//let radius;
-//let circumference;
-
-//radius=window.prompt(`Enter the radius of a circle`);
-//radius=Number(radius);
-
-//circumference=2 * pi * radius;
-
-//document.getElementById("mySubmit").onclick=function(){
-//radius =document.getElementById("myText").value;
-//radius=Number(radius);
-//circumference=2 * PI * radius;
-//document.getElementById("myH3").textContent=circumference+"cm";
-
-const decreaseBtn=document.getElementById("decreaseBtn");
-const resetBtn=document.getElementById("resetBtn");
-const increaseBtn=document.getElementById("increaseBtn");
-const countLabel=document.getElementById("countLabel");
-let count=0;
-
-increaseBtn.onclick=function(){
-    count++;
-    countLabel.textContent=count;
-}
-
-decreaseBtn.onclick=function(){
-    count--;
-    countLabel.textContent=count;
-}
-
-resetBtn.onclick=function(){
-    count=0;;
-    countLabel.textContent=count;
+myButton.onclick=function(){
+    randomNum1=Math.floor(Math.random()*max)+min;
+    randomNum2=Math.floor(Math.random()*max)+min;
+    randomNum3=Math.floor(Math.random()*max)+min;
+    myLabel.textContent=randomNum1;
+     myLabe2.textContent=randomNum2;
+      myLabe3.textContent=randomNum3;
 }
